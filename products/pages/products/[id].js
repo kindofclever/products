b56ddf9264921b309e8ae40d98592bf8e4d13924
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-10-24 17:50:42 +0200

    Fized id Update in URL

--- products/pages/products/[id].js
+++ products/pages/products/[id].js
@@ -2,17 +2,26 @@ import Head from "next/head";
 import Image from "next/image";
 import { products } from "../../data/data.js";
 import { useRouter } from "next/router.js";
+import { useEffect, useState } from "react";
 
 const product = () => {
-	const router = useRouter()
-	const { id } = router.query
-    const product = products.find(element => element.id = id)
-    console.log(product, id)
-return (
+	const router = useRouter();
+	var { id } = router.query;
+    console.log(id)
+    if (!id){
+        return 
+        <></>;
+    }
+    var product = products.find((element) => (element.id = id));  
+        return (
+            <ul > 
+                <ul>{id}</ul>               
+                <ul> {product.title} </ul>
+                <ul> Price: {product.price} </ul>
+                <ul> Description: {product.description}</ul>
+            </ul>
+        );
+    }
 
-	<></>
-)
-	
-}
 
 export default product;
